ea66c90298794a55b2b05aec0abf9c4200d411f2
VuThanhSang <zimb240@gmail.com>
2023-02-12 23:14:24 +0700

    ...

---
+++ server/src/routes/v1/follow.route.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const verifyToken = require("../../middlewares/verifyToken");
+const router = express.Router();
+const followController = require(".../../follow.route");
+
+router.route("");
